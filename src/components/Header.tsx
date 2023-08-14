a59493acf467525bd8360b5db81786c26adfac07
NikaBotchorishvili <nikabotchorishvili@gmail.com>
2023-08-14 15:20:18 +0400

    image positioning enhanced

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,64 +5,67 @@ function Header() {
 	const [toggled, setToggled] = useState(false);
 
 	return (
-		<header className="flex w-[80vw]  justify-between items-center mx-auto mt-10 overflow-x-hidden">
-			<div className="fixed">
-				<img src="/logo.svg" alt="" />
-			</div>
-			<motion.nav
-				initial={{ top: -200, opacity: "var(--opacity-from)" }}
-				animate={{
-					top: toggled ? 100 : -200,
-					opacity: toggled
-						? "var(--opacity-to)"
-						: "var(--opacity-from)",
-				}}
-				className="flex flex-col md:flex-row md:h-fit bg-white p-8 md:p-0 md:bg-none shadow-2xl w-[90vw] md:w-fit left-1/2 -translate-x-1/2 top-20  md:translate-x-0 fixed z-50  md:static md:gap-x-20 items-center gap-y-8 text-center 
+		<>
+			<span className="absolute w-[50vw] h-[50vh] md:h-[60vh] bg-lightGrayishBlue rounded-bl-[50px] -z-20 right-0"></span>
+			<header className="flex w-[80vw]  justify-between items-center mx-auto mt-10 overflow-x-hidden">
+				<div className="fixed md:static">
+					<img src="/logo.svg" alt="" />
+				</div>
+				<motion.nav
+					initial={{ top: -200, opacity: "var(--opacity-from)" }}
+					animate={{
+						top: toggled ? 100 : -200,
+						opacity: toggled
+							? "var(--opacity-to)"
+							: "var(--opacity-from)",
+					}}
+					className="flex flex-col md:flex-row md:h-fit md:bg-opacity-0 bg-white p-8 md:p-0 shadow-2xl w-[90vw] md:w-fit left-1/2 -translate-x-1/2 top-20  md:translate-x-0 fixed z-50  md:static md:gap-x-20 items-center gap-y-8 text-center 
 				[--opacity-from:0%] 
 				[--opacity-to:100%] 
 				md:[--opacity-from:100%] 
 				md:[--opacity-to:100%]"
-			>
-				<ul className="flex md:flex-row md:w-fit w-[200px] flex-col gap-y-5 md:gap-x-10">
-					<li>
-						<a
-							className="font-barlowCondensed text-veryDarkBlue font-bold hover:underline"
-							href="#"
-						>
-							PRODUCT
-						</a>
-					</li>
-					<li>
-						<a
-							className="font-barlowCondensed text-veryDarkBlue font-bold hover:underline"
-							href="#"
-						>
-							FEATURES
-						</a>
-					</li>
-					<li>
-						<a
-							className="font-barlowCondensed text-veryDarkBlue font-bold hover:underline"
-							href="#"
-						>
-							PRICING
-						</a>
-					</li>
-				</ul>
-				<a
-					className="font-barlowCondensed md:border-none border-t border-t-darkGrayishBlue md:w-fit w-[90%] text-darkGrayishBlue font-bold hover:underline"
-					href="#"
 				>
-					LOGIN
-				</a>
-			</motion.nav>
-			<img
-				src={toggled ? "icon-close.svg" : "icon-hamburger.svg"}
-				className="w-[20px] h-[20px] md:hidden z-50 fixed right-10"
-				alt=""
-				onClick={() => setToggled((prev) => !prev)}
-			/>
-		</header>
+					<ul className="flex md:flex-row md:w-fit w-[200px] flex-col gap-y-5 md:gap-x-10">
+						<li>
+							<a
+								className="font-barlowCondensed text-veryDarkBlue font-bold hover:underline"
+								href="#"
+							>
+								PRODUCT
+							</a>
+						</li>
+						<li>
+							<a
+								className="font-barlowCondensed text-veryDarkBlue font-bold hover:underline"
+								href="#"
+							>
+								FEATURES
+							</a>
+						</li>
+						<li>
+							<a
+								className="font-barlowCondensed text-veryDarkBlue font-bold hover:underline"
+								href="#"
+							>
+								PRICING
+							</a>
+						</li>
+					</ul>
+					<a
+						className="font-barlowCondensed md:border-none border-t border-t-darkGrayishBlue md:w-fit w-[90%] text-darkGrayishBlue font-bold hover:underline"
+						href="#"
+					>
+						LOGIN
+					</a>
+				</motion.nav>
+				<img
+					src={toggled ? "icon-close.svg" : "icon-hamburger.svg"}
+					className="w-[20px] h-[20px] md:hidden z-50 md:static fixed right-10"
+					alt=""
+					onClick={() => setToggled((prev) => !prev)}
+				/>
+			</header>
+		</>
 	);
 }
 
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,7 +1,7 @@
 function Home() {
 	return (
 		<main className="flex md:flex-row flex-col-reverse mt-24 w-[80vw] gap-y-20 justify-between mx-auto items-center">
-			<section className="flex flex-col gap-y-6">
+			<section className="flex flex-col justify-start gap-y-6">
 				<h3 className=" flex gap-x-2 items-center">
 					<mark className="text-xl bg-veryDarkBlue text-lightGrayishBlue p-1 rounded-lg font-bold font-barlowCondensed">
 						NEW
@@ -10,24 +10,28 @@ function Home() {
 						MONOGRAPH DASHBOARD
 					</span>
 				</h3>
-				<h1 className="lg:text-6xl text-4xl text-veryDarkBlue font-barlowCondensed  font-bold md:max-w-md">
+				<h1 className="lg:text-5xl text-3xl text-veryDarkBlue font-barlowCondensed  font-bold md:max-w-md">
 					POWERFUL INSIGHTS INTO YOUR TEAM
 				</h1>
 				<p className="red text-xl md:max-w-xs text-darkGrayishBlue">
 					Project planning and time tracking for agile teams
 				</p>
 
-				<div className="flex gap-x-4 items-center mx-auto">
+				<div className="flex gap-x-4 items-center">
 					<button className="text-lightGrayishBlue bg-red font-bold font-barlowCondensed rounded-lg sm:text-lg tracking-wide px-6 py-2 hover:bg-opacity-80">
 						SCHEDULE A DEMO
 					</button>
-					<small className="tracking-widest text-grayishBlue sm:text-lg  ">
+					<small className="tracking-widest text-grayishBlue sm:text-lg">
 						TO SEE A PREVIEW
 					</small>
 				</div>
 			</section>
-			<section className="relative -right-[80px] md:-right-1/3">
-				<img src="illustration-devices.svg" className="w-max h-[200px] md:h-[300px] md:w-max " alt="" />
+			<section className="relative -right-1/4 sm:-right-1/3">
+				<img
+					src="illustration-devices.svg"
+					className="h-[250px] md:h-[300px] md:w-max "
+					alt="illustration image"
+				/>
 			</section>
 		</main>
 	);
